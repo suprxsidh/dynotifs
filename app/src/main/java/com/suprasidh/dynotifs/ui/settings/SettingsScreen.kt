17dc128a25a7114e7a8e54f821653efe0c64c3c1
Dynotifs Developer <developer@example.com>
2026-04-18 02:25:36 +0530

    fix: missing Composable import

--- app/src/main/java/com/suprasidh/dynotifs/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/suprasidh/dynotifs/ui/settings/SettingsScreen.kt
@@ -14,7 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
-import androidx.compose.material3.Composable
+import androidx.compose.runtime.Composable
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
